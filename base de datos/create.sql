47ffacfa9a9a9bba7fb73c39f80a96d38a47241b
Salvador <ssandovalg2100@alumno.ipn.mx>
2025-06-22 08:55:40 -0600

    Update create.sql

--- base de datos/create.sql
+++ base de datos/create.sql
@@ -1,76 +1,98 @@
--- ===================================
---   Esquema Estrella Data Warehouse
---           (PostgreSQL)
--- ===================================
+CREATE SEQUENCE th01_cat_estado_paciente_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
--- ===================================
---   th01_cat_estado_paciente
--- ===================================
+CREATE SEQUENCE th02_dim_paciente_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE th03_dim_hospital_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE th04_dim_medico_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE th05_cat_estado_cita_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE th06_dim_fecha_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+CREATE SEQUENCE th07_cita_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
 CREATE TABLE th01_cat_estado_paciente
 (
-    id_estado     SERIAL PRIMARY KEY,
+    id_estado     INT DEFAULT nextval('th01_cat_estado_paciente_seq') PRIMARY KEY,
     nombre_estado VARCHAR(100) NOT NULL
 );
 
--- ===================================
---   th02_dim_paciente
--- ===================================
 CREATE TABLE th02_dim_paciente
 (
-    id_paciente      SERIAL PRIMARY KEY,
+    id_paciente      INT DEFAULT nextval('th02_dim_paciente_seq') PRIMARY KEY,
     nombre_completo  VARCHAR(150),
     fecha_afiliacion DATE,
     id_estado        INT REFERENCES th01_cat_estado_paciente (id_estado)
 );
 
--- ===================================
---   th03_dim_hospital
--- ===================================
 CREATE TABLE th03_dim_hospital
 (
-    id_hospital     SERIAL PRIMARY KEY,
+    id_hospital     INT DEFAULT nextval('th03_dim_hospital_seq') PRIMARY KEY,
     nombre          VARCHAR(255),
     estado_hospital VARCHAR(100)
 );
 
--- ===================================
---   th04_dim_medico
--- ===================================
 CREATE TABLE th04_dim_medico
 (
-    id_medico       SERIAL PRIMARY KEY,
+    id_medico       INT DEFAULT nextval('th04_dim_medico_seq') PRIMARY KEY,
     nombre_completo VARCHAR(255),
     estado_medico   VARCHAR(100)
 );
 
--- ===================================
---   th05_cat_estado_cita
--- ===================================
 CREATE TABLE th05_cat_estado_cita
 (
-    id_estado     SERIAL PRIMARY KEY,
+    id_estado     INT DEFAULT nextval('th05_cat_estado_cita_seq') PRIMARY KEY,
     nombre_estado VARCHAR(100) NOT NULL
 );
 
--- ===================================
---   th06_dim_fecha
--- ===================================
 CREATE TABLE th06_dim_fecha
 (
-    id_fecha SERIAL PRIMARY KEY,
+    id_fecha INT DEFAULT nextval('th06_dim_fecha_seq') PRIMARY KEY,
     fecha    DATE
 );
 
--- ===================================
---   th07_cita
--- ===================================
 CREATE TABLE th07_cita
 (
-    id_cita        SERIAL PRIMARY KEY,
+    id_cita        INT DEFAULT nextval('th07_cita_seq') PRIMARY KEY,
     fk_id_fecha    INT REFERENCES th06_dim_fecha (id_fecha),
     fk_id_hospital INT REFERENCES th03_dim_hospital (id_hospital),
     fk_id_medico   INT REFERENCES th04_dim_medico (id_medico),
     fk_id_paciente INT REFERENCES th02_dim_paciente (id_paciente),
     fk_id_estado   INT REFERENCES th05_cat_estado_cita (id_estado)
 );
-
