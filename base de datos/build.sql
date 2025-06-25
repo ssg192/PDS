35598e54ad6915f97d55052b180f794b9b2bc31f
Salvador <ssandovalg2100@alumno.ipn.mx>
2025-06-25 03:52:20 -0600

    Update build.sql

--- base de datos/build.sql
+++ base de datos/build.sql
@@ -4,13 +4,8 @@ WHERE datname = 'hospital';
 
 DROP
 DATABASE IF EXISTS "hospital";
-
-CREATE DATABASE hospital
-    WITH ENCODING 'UTF8'
-    LC_COLLATE='es_MX.utf8'
-    LC_CTYPE='es_MX.utf8'
-    TEMPLATE=template0;
-
+CREATE
+DATABASE "hospital";
 BEGIN;
 \c hospital
 \i create.sql
